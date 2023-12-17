3a9d9caccd7abd6026d65aa49aade61def671789
Roman Khabayev <supernaturalmlady@gmail.com>
2023-12-17 23:39:36 +0600

    wewindows. some refactoring, jwt tokens creation, little fixes

---
+++ server/routers/user_router.py
@@ -0,0 +1,37 @@
+import fastapi as _fastapi
+from fastapi import APIRouter
+import fastapi.security as _security
+
+import sqlalchemy.orm as _orm
+
+import services.user_service as _user_service
+import services.token_service as _token_service
+
+import schemas as _schemas
+
+router = APIRouter()
+
+
+@router.post('/api/users')
+async def create_user(user: _schemas.UserCreate, db: _orm.Session = _fastapi.Depends(_user_service.get_db)):
+    try:
+        db_user = await _user_service.get_user_by_email(user.email, db)
+
+        if db_user:
+            raise _fastapi.HTTPException(status_code=400, detail='Email already in use')
+
+        user = await _user_service.create_user(user, db)
+        return await _token_service.create_token(user)
+    except Exception as err:
+        return err
+
+
+@router.post('/api/token')
+async def generate_token(form_data: _security.OAuth2PasswordRequestForm = _fastapi.Depends(),
+                         db: _orm.Session = _fastapi.Depends(_user_service.get_db)):
+    user = await _user_service.auth_user(form_data.username, form_data.password, db)
+
+    if not user:
+        raise _fastapi.HTTPException(status_code=401, detail='Invalid credentials')
+
+    return await _token_service.create_token(user)
