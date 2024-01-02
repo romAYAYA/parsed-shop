d5794c8f279c985d38ec2c66ec297e24279cefa0
Roman Khabayev <supernaturalmlady@gmail.com>
2024-01-02 18:27:10 +0600

    modal window on buy button added

--- client/src/components/ProductComponent.vue
+++ client/src/components/ProductComponent.vue
@@ -13,14 +13,32 @@
     </div>
     <div class="flex align-items-center justify-content-between">
       <span class="text-xl font-semibold">{{ product.price }}</span>
-      <Button icon="pi pi-shopping-cart" rounded></Button>
+      <Button
+        @click="visible = true"
+        icon="pi pi-shopping-cart"
+        rounded
+      ></Button>
+      <Dialog
+        v-model:visible="visible"
+        modal
+        :header="product.name"
+        :style="{ width: '50rem' }"
+        :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
+      >
+        <p>Ya nejnau kak prodatt ribov, sorry</p>
+      </Dialog>
     </div>
   </div>
 </template>
 
 <script setup>
 import Button from 'primevue/button'
+import Dialog from 'primevue/dialog'
+
+import { ref } from 'vue'
 
 const { product } = defineProps(['product'])
 const { name, img, price } = product
+
+const visible = ref(false)
 </script>
